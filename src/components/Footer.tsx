0da25ea1b7615a4037b6d6705400d73a01274fa0
DiegoRojo99 <diego99rojo@gmail.com>
2025-01-27 23:28:55 +0000

    Updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
 
 export default function Footer(){
   return (
-    <footer className="bg-gray-800 text-white p-4 mt-auto">
-    <p className="text-center">&copy; 2025 Stark Library. All rights reserved.</p>
-    <p className="text-center">Data provided by Marvel. &copy; 2014 Marvel</p>
-  </footer>
+    <footer className="bg-black text-white p-4 mt-auto flex flex-col">
+      <p className="text-center">
+        &copy; 2025 Stark Library. All rights reserved.
+      </p>
+      <Link className="text-center" href={"http://marvel.com"}>
+        Data provided by Marvel. &copy; 2025 MARVEL
+      </Link>
+    </footer>
   )
 }
