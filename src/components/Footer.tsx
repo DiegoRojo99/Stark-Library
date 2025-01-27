c23c8510366c3d68cb2d54740c9c3adf6a890e5c
DiegoRojo99 <diego99rojo@gmail.com>
2025-01-27 21:54:46 +0000

    Added nav and footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+
+export default function Footer(){
+  return (
+    <footer className="bg-gray-800 text-white p-4 mt-auto">
+    <p className="text-center">&copy; 2025 Stark Library. All rights reserved.</p>
+    <p className="text-center">Data provided by Marvel. &copy; 2014 Marvel</p>
+  </footer>
+  )
+}
---
+++ src/components/Nav.tsx
@@ -0,0 +1,8 @@
+
+export default function Nav(){
+  return (
+    <nav className="bg-gray-800 text-white p-4">
+      <h1 className="text-xl text-center">Stark Library</h1>
+    </nav>
+  )
+}
