649df81d2ea30572a14848ab3c17036567a7104b
DiegoRojo99 <diego99rojo@gmail.com>
2025-01-27 22:43:46 +0000

    Added in memory cache for characters

--- src/lib/marvelAPI.ts
+++ src/lib/marvelAPI.ts
@@ -3,6 +3,9 @@ import crypto from 'crypto';
 const publicKey = process.env.MARVEL_PUBLIC_KEY!;
 const privateKey = process.env.MARVEL_PRIVATE_KEY!;
 
+const cache = new Map<string, { data: any; timestamp: number }>();
+const CACHE_EXPIRATION_TIME = 86400000;
+
 export const fetchMarvelData = async (endpoint: string, queryParams: Record<string, string>) => {
   const ts = Date.now().toString();
   const hash = crypto
@@ -11,11 +14,21 @@ export const fetchMarvelData = async (endpoint: string, queryParams: Record<stri
     .digest('hex');
 
   const queryString = new URLSearchParams(queryParams).toString();
-  const url = `https://gateway.marvel.com/v1/public/${endpoint}?${queryString}&ts=${ts}&apikey=${publicKey}&hash=${hash}`;
+  const urlWithoutDynamicParams = `https://gateway.marvel.com/v1/public/${endpoint}?${queryString}`;
+  const cacheKey = crypto.createHash('md5').update(urlWithoutDynamicParams).digest('hex');
+  
+  const cached = cache.get(cacheKey);
+  if (cached && Date.now() - cached.timestamp < CACHE_EXPIRATION_TIME) {
+    return cached.data;
+  }
 
   try {
+    const url = `${urlWithoutDynamicParams}&ts=${ts}&apikey=${publicKey}&hash=${hash}`;
     const res = await fetch(url);
     const data = await res.json();
+
+    cache.set(cacheKey, { data, timestamp: Date.now() });
+
     return data;
   } 
   catch (error) {
