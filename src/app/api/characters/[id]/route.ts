8141ec75eff925df267aca63c1d4c8cb4f9d5a14
DiegoRojo99 <diego99rojo@gmail.com>
2025-01-28 22:09:14 +0000

    GET character endpoint pulls data and comics

---
+++ src/app/api/characters/[id]/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server';
+import {fetchMarvelData} from '@/utils/marvelAPI';
+import { context } from '@/app/types/routes';
+
+export async function GET(req: Request, context: context) {
+  const { id } = await context.params;
+
+  if (!id) {
+    return NextResponse.json({ error: 'Character ID is required.' }, { status: 400 });
+  }
+
+  try {
+    const characterData = await fetchMarvelData(`characters/${id}`, {});
+    const comicsData = await fetchMarvelData(`characters/${id}/comics`, {});
+
+    const character = characterData.data.results[0];
+    const comics = comicsData.data.results;
+
+    const data = {
+      character,
+      comics
+    }
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error: 'Failed to fetch character details.' }, { status: 500 });
+  }
+}
